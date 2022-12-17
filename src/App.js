653f4908173654d6b180078a1db70e5a33402883
fahime <fahimeh757@gmail.com>
2022-12-17 18:51:09 +0330

    disable line

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ function App() {
     result = filterCategory(result);
     result = sortData(result);
     setFilteredProducts(result);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [products, sort, searchValue, selectedCategory]);
   useEffect(() => {
     const storedProductsData =
